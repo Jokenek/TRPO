45b1ed6b095e1ed61df1199c5088d352f717a701
Jokenek <fredi4ik99@gmail.com>
2021-02-25 21:50:56 +0700

    3 день
    В векторе сделано Умножение вектора на матрицу,  Скалярное произведение, Вычисление длины вектора,  Проверка сонаправленности векторов, Проверка векторов на ортогональность

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 import pandas as pd
+from math import sqrt
 
 Form, Window = uic.loadUiType("1_2.ui")
 
@@ -12,27 +13,27 @@ window.show()
 
 #Все операции скаляра
 class Skalar:
-    def summAB():
-        dataA=form.aSkolar.value()
-        dataB=form.bSkolar.value()
-        A=dataA+dataB
-        form.textBrowser.setText(str(A))
-    def _A():
-        dataA = form.aSkolar.value()
-        A =-dataA
-        form.textBrowser.setText(str(A))
-    def AB():
+    def summAB(self):
         dataA = form.aSkolar.value()
         dataB = form.bSkolar.value()
-        A =dataA*dataB
+        A = dataA+dataB
         form.textBrowser.setText(str(A))
-    def AstepenB():
+    def _A (self):
+        dataA = form.aSkolar.value()
+        A = -dataA
+        form.textBrowser.setText(str(A))
+    def AB(self):
+        dataA = form.aSkolar.value()
+        dataB = form.bSkolar.value()
+        A = dataA * dataB
+        form.textBrowser.setText(str(A))
+    def AstepenB(self):
         dataA = form.aSkolar.value()
         dataB = form.bSkolar.value()
         A =dataA**dataB
         form.textBrowser.setText(str(A))
 
-    def korenAB():
+    def korenAB(self):
         dataA = form.aSkolar.value()
         dataB = form.bSkolar.value()
         A = dataA ** (1/dataB)
@@ -41,40 +42,40 @@ class Skalar:
 
 #Все операции матриц
 class Ar:
-    def arrayAB():
+    def arrayAB(self):
         dataA = pd.read_csv("a.csv", sep=";", header=None)
         dataB = pd.read_csv("b.csv", sep=";", header=None)
-        a=dataA.shape
-        b=dataB.shape
-        m=a[1]
-        n=b[0]
-        k=a[0]
-        g=0
+        a = dataA.shape
+        b = dataB.shape
+        m = a[1]
+        n = b[0]
+        k = a[0]
+        g = 0
         f = open('end.csv', 'w')
         for j in range(n):
             for i in range(m):
                 for l in range(k):
                     g += dataA[i][l]*dataB[l][j]
-                if i==(m-1):
+                if i == (m-1):
                     f.write(str(g))
                 else:
                     f.write(str(g) + ' ;')
-                g=0
+                g = 0
             f.write('\n')
         f.close()
         dataEND = pd.read_csv("end.csv", sep=";", header=None)
         form.textBrowser_2.setText(str(dataEND))
-    def arrayASkalar():
+    def arrayASkalar(self):
         dataA = pd.read_csv("a.csv", sep=";", header=None)
         skalar = form.doubleSpinBox.value()
-        a=dataA.shape
-        m=a[1]
-        n=a[0]
+        a = dataA.shape
+        m = a[1]
+        n = [0]
         f = open('end.csv', 'w')
         for j in range(n):
             for i in range(m):
                 g = dataA[i][j]*skalar
-                if i==(m-1):
+                if i == (m-1):
                     f.write(str(g))
                 else:
                     f.write(str(g) + ' ;')
@@ -82,7 +83,7 @@ class Ar:
         f.close()
         dataEND = pd.read_csv("end.csv", sep=";", header=None)
         form.textBrowser_2.setText(str(dataEND))
-    def arrayAsummB():
+    def arrayAsummB(self):
         dataA = pd.read_csv("a.csv", sep=";", header=None)
         dataB = pd.read_csv("b.csv", sep=";", header=None)
         a=dataA.shape
@@ -92,7 +93,7 @@ class Ar:
         for j in range(n):
             for i in range(m):
                 g = dataA[i][j]+dataB[i][j]
-                if i==(m-1):
+                if i == (m-1):
                     f.write(str(g))
                 else:
                     f.write(str(g) + ' ;')
@@ -100,17 +101,17 @@ class Ar:
         f.close()
         dataEND = pd.read_csv("end.csv", sep=";", header=None)
         form.textBrowser_2.setText(str(dataEND))
-    def arrayAmullB():
+    def arrayAmullB(self):
         dataA = pd.read_csv("a.csv", sep=";", header=None)
         dataB = pd.read_csv("b.csv", sep=";", header=None)
-        a=dataA.shape
-        m=a[1]
-        n=a[0]
+        a = dataA.shape
+        m = a[1]
+        n = a[0]
         f = open('end.csv', 'w')
         for j in range(n):
             for i in range(m):
                 g = dataA[i][j]*dataB[i][j]
-                if i==(m-1):
+                if i == (m-1):
                     f.write(str(g))
                 else:
                     f.write(str(g) + ' ;')
@@ -118,18 +119,18 @@ class Ar:
         f.close()
         dataEND = pd.read_csv("end.csv", sep=";", header=None)
         form.textBrowser_2.setText(str(dataEND))
-    def arrayAVector():
+    def arrayAVector(self):
         dataA = pd.read_csv("a.csv", sep=";", header=None)
         dataB = pd.read_csv("b.csv", sep=";", header=None)
-        b=dataB.values.tolist()[0]
-        a=dataA.shape
-        m=a[1]
-        n=a[0]
+        b = dataB.values.tolist()[0]
+        a = dataA.shape
+        m = a[1]
+        n = a[0]
         f = open('end.csv', 'w')
         for j in range(n):
             for i in range(m):
                 g = dataA[i][j]*b[i]
-                if i==(m-1):
+                if i == (m-1):
                     f.write(str(g))
                 else:
                     f.write(str(g) + ' ;')
@@ -137,15 +138,14 @@ class Ar:
         f.close()
         dataEND = pd.read_csv("end.csv", sep=";", header=None)
         form.textBrowser_2.setText(str(dataEND))
-    def arrayATr():
+    def arrayATr(self):
         dataA = pd.read_csv("a.csv", sep=";", header=None)
-        a=dataA.shape
         dataA.T.to_csv('end.csv',sep =';',header = False, index = False)
         form.textBrowser_2.setText(str(dataA.T))
 
 #Все операции вектора
 class Vector:
-    def vectorSkalar():
+    def vectorSkalar(self):
         dataA = pd.read_csv("b.csv", sep=";", header=None)
         skalar = form.doubleSpinBox_2.value()
         b = dataA.values.tolist()[0]
@@ -162,7 +162,7 @@ class Vector:
         f.close()
         dataEND = pd.read_csv("end.csv", sep=";", header=None)
         form.textBrowser_3.setText(str(dataEND))
-    def vectorSumm():
+    def vectorSumm(self):
         dataA = pd.read_csv("a.csv", sep=";", header=None)
         a = dataA.values.tolist()[0]
         dataB = pd.read_csv("b.csv", sep=";", header=None)
@@ -180,7 +180,7 @@ class Vector:
         f.close()
         dataEND = pd.read_csv("end.csv", sep=";", header=None)
         form.textBrowser_3.setText(str(dataEND))
-    def vectorMull():
+    def vectorMull(self):
         dataA = pd.read_csv("a.csv", sep=";", header=None)
         a = dataA.values.tolist()[0]
         dataB = pd.read_csv("b.csv", sep=";", header=None)
@@ -198,6 +198,91 @@ class Vector:
         f.close()
         dataEND = pd.read_csv("end.csv", sep=";", header=None)
         form.textBrowser_3.setText(str(dataEND))
+    def vectorArray(self):
+        dataA = pd.read_csv("a.csv", sep=";", header=None)
+        dataB = pd.read_csv("b.csv", sep=";", header=None)
+        b = dataA.values.tolist()[0]
+        a = dataB.shape
+        m = a[1]
+        n = a[0]
+        f = open('end.csv', 'w')
+        for j in range(n):
+            for i in range(m):
+                g = dataB[i][j] * b[i]
+                if i == (m - 1):
+                    f.write(str(g))
+                else:
+                    f.write(str(g) + ' ;')
+            f.write('\n')
+        f.close()
+        dataEND = pd.read_csv("end.csv", sep=";", header=None)
+        form.textBrowser_3.setText(str(dataEND))
+    def SkalarProizv(self):
+        dataA = pd.read_csv("a.csv", sep=";", header=None)
+        a = dataA.values.tolist()[0]
+        dataB = pd.read_csv("b.csv", sep=";", header=None)
+        b = dataB.values.tolist()[0]
+        l = dataA.shape
+        n = l[1]
+        g = 0
+        for i in range(n):
+            g += a[i]*b[i]
+        form.textBrowser_3.setText(str(g))
+    def DlinnaVectora(self):
+        dataA = pd.read_csv("a.csv", sep=";", header=None)
+        a = dataA.values.tolist()[0]
+        l = dataA.shape
+        n = l[1]
+        g = 0
+        for i in range(n):
+            g += a[i]*a[i]
+        g = sqrt(g)
+        form.textBrowser_3.setText(str(g))
+    def CoNapravlen(self):
+        dataA = pd.read_csv("a.csv", sep=";", header=None)
+        a = dataA.values.tolist()[0]
+        dataB = pd.read_csv("b.csv", sep=";", header=None)
+        b = dataB.values.tolist()[0]
+        l = dataA.shape
+        n = l[1]
+        f = open('end.csv', 'w')
+        for i in range(n):
+            try:
+                g = a[i] / b[i]
+            except ZeroDivisionError:
+                g = 0
+            if i == (n - 1):
+                f.write(str(g))
+            else:
+                f.write(str(g) + ' ;')
+        f.write('\n')
+        f.close()
+        dataEND = pd.read_csv("end.csv", sep=";", header=None)
+        end = dataEND.values.tolist()[0]
+        for i in range(n-1):
+            if(end[i]!=end[i+1]):
+                form.textBrowser_3.setText('Данные вектора не сонаправленные')
+                break
+            if (end[i]<0):
+                form.textBrowser_3.setText('Данные вектора не сонаправленные')
+                break
+            if (i==n-2):
+                form.textBrowser_3.setText('Данные вектора сонаправленные')
+    def Ortogon(self):
+        dataA = pd.read_csv("a.csv", sep=";", header=None)
+        a = dataA.values.tolist()[0]
+        dataB = pd.read_csv("b.csv", sep=";", header=None)
+        b = dataB.values.tolist()[0]
+        l = dataA.shape
+        n = l[1]
+        g = 0
+        for i in range(n):
+            g += a[i] * b[i]
+        if (g == 0):
+            form.textBrowser_3.setText('Вектора ортогональны, так как их скалярное произведение равно нулю.')
+        else:
+            form.textBrowser_3.setText('Вектора не ортогональны, так как их скалярное произведение не равно нулю.')
+
 #Вызов метода в форме
 form.Summ.clicked.connect(Skalar.summAB)
 form.Inver.clicked.connect(Skalar._A)
@@ -216,4 +301,9 @@ form.TArray.clicked.connect(Ar.arrayATr)
 form.SkolarVector.clicked.connect(Vector.vectorSkalar)
 form.SummVector.clicked.connect(Vector.vectorSumm)
 form.MullVector.clicked.connect(Vector.vectorMull)
+form.MullVectorArray.clicked.connect(Vector.vectorArray)
+form.MullVectorVector.clicked.connect(Vector.SkalarProizv)
+form.DilinnaVectora.clicked.connect(Vector.DlinnaVectora)
+form.VectoSoNoprav.clicked.connect(Vector.CoNapravlen)
+form.OrtoganalVectora.clicked.connect(Vector.Ortogon)
 app.exec()
